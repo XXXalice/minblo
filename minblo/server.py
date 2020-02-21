b7e62ea2dd1929d82d0e0a8b5a0bf74b0ea303b2
梅宮 <euthanasia045@gmail.com>
2020-02-21 22:41:40 +0900

    add server, templates, handler/

---
+++ minblo/server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template, request, url_for, jsonify
+
+def startup_server():
+    app = Flask(__name__) # minblo server
+
+    @app.route('/')
+    def index():
+        # root page
+        pass
+
+
+if __name__ == '__main__':
+    startup_server()
+else:
+    import sys
+    sys.stdout.write("Can not refer this scripts from other programs. \nplz run directly.")
+
