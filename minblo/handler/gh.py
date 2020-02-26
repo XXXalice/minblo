232e14e0bf3d354dfe3789691e96d3a0f64ba849
梅宮 <euthanasia045@gmail.com>
2020-02-26 12:55:35 +0900

    add get_issues() in gh.py

--- minblo/handler/gh.py
+++ minblo/handler/gh.py
@@ -1,5 +1,6 @@
 import yaml
 import inspect
+import requests
 import sys
 import os
 
@@ -17,7 +18,20 @@ class GithubApis:
         config_path = os.path.join('/'.join(inspect.stack()[0][1].split('/')[:-1]), CONFIG_FILE_NAME) # handler/:CONFIG_FILE_NAME
         with open(config_path, 'r+') as f:
             self.config = yaml.load(f) # return type dict
+        self.api_uri =  "https://api.github.com/repos/{}/{}/"
 
+    def get_issues(self):
+        self.api_uri = self.api_uri.format(
+            self.config["github"]["id"],
+            self.config["github"]["repo"]
+        ) + "issues"
+        try:
+            raw_issues_data = requests.get(self.api_uri)
+        except Exception as e:
+            print(e)
+            return
+
+        return raw_issues_data
 
 
 
