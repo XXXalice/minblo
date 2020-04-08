ba3294c6b67084935ffea1b0a7d4487681436322
梅宮 <euthanasia045@gmail.com>
2020-04-08 22:57:45 +0900

    fix Flask run() bugs.

--- minblo/server.py
+++ minblo/server.py
@@ -1,29 +1,31 @@
 from flask import Flask
 from flask import render_template, request, url_for, jsonify
-from .handler import expert as ex
+from handler import expert as ex
 import random
 
-def startup_server():
-    app = Flask(__name__) # minblo server
-
-    @app.route('/')
-    def index():
-        # root page
-        pass
-
-    @app.route('/posts/<int:post_num>', methods=['GET'])
-    def blogs(post_num):
-        return post_num
+app = Flask(__name__) # minblo server
 
 
-    @app.route('/ume', methods=['GET'])
-    def umemiya():
-        expert = ex.Expert(class_id=random.randint(256))
+@app.route('/')
+def index():
+    # root page
+    return "hello!"
 
+@app.route('/posts/<int:post_num>', methods=['GET'])
+def blogs(post_num):
+    return str(post_num)
+
+
+@app.route('/ume', methods=['GET'])
+def umemiya():
+    expert = ex.Expert(class_id=random.randint(256))
+
+
+def startup_server(debug=True):
+    app.run(debug=debug)
 
 if __name__ == '__main__':
     startup_server()
 else:
     import sys
     sys.stdout.write("Can not refer this scripts from other programs. \nplz run directly.")
-
